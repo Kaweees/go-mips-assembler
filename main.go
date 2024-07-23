bae88b7c512d15a7ef826d770ab07a0d6b2df3db
Miguel Villa Floran <miguel.villafloran@gmail.com>
2024-07-22 20:27:51 -0700

    Scanner loop

--- main.go
+++ main.go
@@ -8,22 +8,42 @@ import (
 )
 
 func main() {
+	var dfa *DFA
+	var err error
+
+	// Initialize the logger
+	initalizeLogger()
+
 	file, err := os.Open("demo.asm")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
+	// Initialize the dfa
+	dfa, err = NewDFA()
+	if err != nil {
+		Log.Error(fmt.Sprintf("Error initializing the request handler: %v", err))
+		return
+	}
+
 	lineCount := 0
 	byteCount := 0
 	scanner := bufio.NewScanner(file)
+
 	for scanner.Scan() {
+		dfa.Reset()
+		for i, r := range scanner.Text() {
+			dfa.Transition(r)
+			// fmt.Print(i, r)
+			fmt.Printf("Index: %d, Rune: %c\n", i, r)
+			byteCount += 1
+		}
 		lineCount += 1
-		byteCount += len(scanner.Text())
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%d lines, %d bytes", lineCount, byteCount)
+	fmt.Printf("%d lines, %d bytes\n", lineCount, byteCount)
 }
